5e524a78c42691318fb5364ad209b5db712a43c4
Chandru <106470788+Chandrut22@users.noreply.github.com>
2023-12-16 05:01:57 +0530

    Update Vertex.java

--- Graph/Vertex.java
+++ Graph/Vertex.java
@@ -24,4 +24,32 @@ public class Vertex{
   public ArrayList<Edge> getEdges(){
     return this.edges;
   }
+
+  public void print(boolean showWeight){
+    String message = "";
+
+    if(this.edges.size() == 0){
+      System.out.println(this.data + " -->");
+      return;
+    }
+
+    for(int i=0;i<this.edges.size();i++){
+      if(i==0){
+        message += this.edges.get(i).getStart.data + " --> ";
+      }
+
+      message += this.edge.get(i).getEnd().data;
+
+      if(showWeight){
+        message += " (" + this.edges.get(i).getWeight() + ")";
+      }
+
+      if(i != this.edges.size() -1){
+        message += ", ";
+      }
+    }
+    System.out.prinln(message);
+  }
+  public static void main (String[] args){
+  }
 }
